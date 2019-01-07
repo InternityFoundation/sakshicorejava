6114b553ae5a61f8ebcfc57925c781e16a885311
sakshi gupta <sakshisg2000@gmail.com>
2019-01-07 22:15:58 +0530

    Add files via upload

---
+++ file1.java
@@ -0,0 +1,18 @@
+package javaDB;
+import java.io.FileOutputStream;  
+
+
+import java.io.*;  
+public class File{    
+public static void main(String args[])throws Exception{    
+     FileOutputStream fout=new FileOutputStream("D:\\testout.txt");    
+     BufferedOutputStream bout=new BufferedOutputStream(fout);    
+     String s="Welcome to javaTpoint.";    
+     byte b[]=s.getBytes();    
+     bout.write(b);    
+     bout.flush();    
+     bout.close();    
+     fout.close();    
+     System.out.println("success");    
+}    
+}  
